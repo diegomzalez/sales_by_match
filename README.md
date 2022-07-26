a1737ba4d8a7ff37d3d2359fe6a2833a0297baff
Diego <diegom.develops@gmail.com>
2022-07-26 19:01:32 -0400

    feat: sales by match
    Problem solved

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sales by Match
+
 I created this repository to show that I have solved this problem from HackerRank :)
--- src/sock_merchant.ts
+++ src/sock_merchant.ts
@@ -4,8 +4,8 @@
  * @param {number[]} ar
  * @returns {number}  The amount of detected pairs  in the array.
  */
-export function sock_merchant(n, ar) {
-  let dict = {};
+export function sock_merchant(n: number, ar: Array<number>): number {
+  let dict: { [key: number]: boolean | undefined } = {};
   let pairs = 0;
   ar.forEach((element) => {
     if (dict[element] === undefined) dict[element] = true;
